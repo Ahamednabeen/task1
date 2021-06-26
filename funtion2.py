bd4a449765efb76cbeefd9ed90e4d051cfd889d7
nabeen <ahamednabeen@gmail.com>
2021-06-27 01:28:10 +0530

    funtions for filter trackbar

---
+++ funtion2.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+def nothing(x):
+    pass
+
+# Create a window
+def initialize_for_filter():
+    cv2.namedWindow('image')
+    # create trackbars for color change
+    cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('SMin','image',0,255,nothing)
+    cv2.createTrackbar('VMin','image',0,255,nothing)
+    cv2.createTrackbar('HMax','image',0,179,nothing)
+    cv2.createTrackbar('SMax','image',0,255,nothing)
+    cv2.createTrackbar('VMax','image',0,255,nothing)
+    # Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('HMax', 'image', 179)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+
+
+
+def get_pos():    
+    hMin = cv2.getTrackbarPos('HMin','image')
+    sMin = cv2.getTrackbarPos('SMin','image')
+    vMin = cv2.getTrackbarPos('VMin','image')
+
+    hMax = cv2.getTrackbarPos('HMax','image')
+    sMax = cv2.getTrackbarPos('SMax','image')
+    vMax = cv2.getTrackbarPos('VMax','image')
+    return hMin,sMin,vMin,hMax,sMax,vMax 
+
+def operation(image,lower,upper):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(image,image, mask= mask)
+    return output
+
+def print_results(hMin,sMin,vMin,hMax,sMax,vMax):
+    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+    # Print if there is a change in HSV value
+    if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
+        print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax        
