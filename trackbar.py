eb2beff753bd23e5b646e8a99d9d5b27c60e2107
nabeen <ahamednabeen@gmail.com>
2021-06-27 01:27:46 +0530

    filter track after splitting functions and main pg

--- trackbar.py
+++ trackbar.py
@@ -2,79 +2,29 @@ import cv2
 import sys
 import numpy as np
 from function import dataloader
+from funtion2 import *
+def main():
+    initialize_for_filter()
+    image = dataloader()
 
-def nothing(x):
-    pass
-image = dataloader()
-# Create a window
-def initialize_for_filter():
-    cv2.namedWindow('image')
-    # create trackbars for color change
-    cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
-    cv2.createTrackbar('SMin','image',0,255,nothing)
-    cv2.createTrackbar('VMin','image',0,255,nothing)
-    cv2.createTrackbar('HMax','image',0,179,nothing)
-    cv2.createTrackbar('SMax','image',0,255,nothing)
-    cv2.createTrackbar('VMax','image',0,255,nothing)
-    # Set default value for MAX HSV trackbars.
-    cv2.setTrackbarPos('HMax', 'image', 179)
-    cv2.setTrackbarPos('SMax', 'image', 255)
-    cv2.setTrackbarPos('VMax', 'image', 255)
+    # Initialize to check if HSV min/max value changes
+    hMin = sMin = vMin = hMax = sMax = vMax = 0
+    output = image
+    wait_time = 33
 
-initialize_for_filter()
+    while(1):
+        hMin,sMin,vMin,hMax,sMax,vMax=get_pos()
+        # Set minimum and max HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
+        output=operation(image,lower,upper)
+        print_results(hMin,sMin,vMin,hMax,sMax,vMax)
+        # Display output image
+        cv2.imshow('image',output)
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+            break
 
-def get_pos():    
-    hMin = cv2.getTrackbarPos('HMin','image')
-    sMin = cv2.getTrackbarPos('SMin','image')
-    vMin = cv2.getTrackbarPos('VMin','image')
-
-    hMax = cv2.getTrackbarPos('HMax','image')
-    sMax = cv2.getTrackbarPos('SMax','image')
-    vMax = cv2.getTrackbarPos('VMax','image')
-    return hMin,sMin,vMin,hMax,sMax,vMax 
-
-def operation(image,lower,upper):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower, upper)
-    output = cv2.bitwise_and(image,image, mask= mask)
-    return output
-
-
-
-
-# Initialize to check if HSV min/max value changes
-
-hMin = sMin = vMin = hMax = sMax = vMax = 0
-output = image
-wait_time = 33
-
-def print_results():
-    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
-    # Print if there is a change in HSV value
-    if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
-        print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
-        phMin = hMin
-        psMin = sMin
-        pvMin = vMin
-        phMax = hMax
-        psMax = sMax
-        pvMax = vMax    
-
-while(1):
-    
-    hMin,sMin,vMin,hMax,sMax,vMax=get_pos()
-    # Set minimum and max HSV values to display
-    lower = np.array([hMin, sMin, vMin])
-    upper = np.array([hMax, sMax, vMax])
-    output=operation(image,lower,upper)
-    print_results()
-
-    # Display output image
-    cv2.imshow('image',output)
-
-    # Wait longer to prevent freeze for videos.
-    if cv2.waitKey(wait_time) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
-
+    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
